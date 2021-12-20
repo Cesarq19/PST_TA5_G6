b5b0adf6c4705e4376a327ac0fc9489e564a72f3
Cesarq19 <cepaquin@espol.edu.ec>
2021-12-20 14:35:44 -0500

    Creacion de Quinta Activity y Sexta Activity

---
+++ app/src/main/java/com/example/pst_ta5_g6/Sexta_Activity.java
@@ -0,0 +1,14 @@
+package com.example.pst_ta5_g6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sexta_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sexta);
+    }
+}
